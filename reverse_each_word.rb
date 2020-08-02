0fda9040727ef38644d3d5945f9b78bc90a331fb
Jonathan Snipe <jon.mike919@gmail.com>
2020-08-02 17:46:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,17 @@
 reversed_words = []
 
+def reverse_each_word(phrase)
+  reversed_words = []
+  split_phrase = phrase.split(" ")
+  
+  counter = 0
+  
+  while counter < split_phrase.size
+    
+    counter += 1
+  end
+  
+end
 
   
   
